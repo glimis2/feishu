790425e95ada31ff46a01a4cb1d040d54d44bcf8
glimis2 <3578823904@qq.com>
2026-05-27 21:34:21 +0800

    chore: downgrade p-limit to v3 for Jest compatibility

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,19 @@ module.exports = {
     'src/**/*.ts',
     '!src/**/*.d.ts'
   ],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true
+      }
+    }]
+  },
   transformIgnorePatterns: [
     'node_modules/(?!(p-limit|yocto-queue)/)'
-  ]
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 };
